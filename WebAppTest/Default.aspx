5ab453fc7f803fad7c838a6251fbe455eccc0254
Per A. Bergersen <pab2222@hotmail.com>
2023-09-11 21:24:40 +0200

    Add project files.

---
+++ WebAppTest/Default.aspx
@@ -0,0 +1,60 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="WebAppTest.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <h1>Din første DB-app</h1>
+        <p>Fullfør denne slik at du har et godt eksempelprosjekt for DB-app! Denne bruker vår åpne databaseserver med
+            adressen glemmen.bergersen.dk,4729
+        <br />
+
+        <br />
+        søk på etternavn<br />
+        <asp:Button ID="ButtonSearchLastName" runat="server" Text="Søk" OnClick="ButtonSearchLastName_Click" />
+        <br />
+        <br />
+        <asp:TextBox ID="TextBoxLastName" runat="server"></asp:TextBox>
+        <hr />
+        <br />
+        søk på fornavn
+
+
+        <br />
+        <asp:Button ID="ButtonSearchFirstName" runat="server" Text="Søk etter fornavn" OnClick="ButtonSearchFirstName_Click" />
+        <br />
+        <asp:TextBox ID="TextBoxFirstName" runat="server"></asp:TextBox>
+        <br />
+        <hr />
+        sett inn ny person<br />
+        Fornavn&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Etternavn&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; PersNr<br />
+        <asp:TextBox ID="TextBoxInsertFirstName" runat="server"></asp:TextBox>
+        <asp:TextBox ID="TextBoxInsertLastName" runat="server"></asp:TextBox>
+        <asp:TextBox ID="TextBoxInsertPersNr" runat="server"></asp:TextBox>
+        <br />
+        <asp:Button ID="ButtonInsertPerson" runat="server" Text="Sett inn ny person" />
+        <br />
+        <br />
+
+
+        <hr />
+        Slett person - Hvilken kolonne skal man ta utgangspunkt i? Lag knapp og tekstboks selv. Husk navnekonvensjoner.<br />
+        DELETE from Person WHERE .... Sjekk w3schools for sql<br />
+        <hr />
+        Oppdatere en person<br />
+        UPDATE... Sjekk w3schools for sql<br /><br />
+
+        Når denne er klar skal den legges på vår webserver.
+        <br />
+        <asp:GridView ID="GridView1" runat="server"></asp:GridView>
+    </form>
+    <p>
+        &nbsp;</p>
+    <p>
+        &nbsp;</p>
+</body>
+</html>
---
+++ WebAppTest/Default.aspx.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebAppTest
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            var connectionString = ConfigurationManager.ConnectionStrings["ConnCms"].ConnectionString;
+            DataTable dt = new DataTable();
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+
+                SqlCommand cmd = new SqlCommand("SELECT * from Person", conn);
+                cmd.CommandType = CommandType.Text;
+
+                SqlDataReader reader = cmd.ExecuteReader();
+                dt.Load(reader);
+                reader.Close();
+                conn.Close();
+            }
+            GridView1.DataSource = dt;
+            GridView1.DataBind();
+
+        }
+
+        protected void ButtonSearchLastName_Click(object sender, EventArgs e)
+        {
+            SqlParameter param;
+            var connectionString = ConfigurationManager.ConnectionStrings["ConnCms"].ConnectionString;
+            DataTable dt = new DataTable();
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+
+                SqlCommand cmd = new SqlCommand("SELECT * from Person where etternavn = @lastName", conn);
+                cmd.CommandType = CommandType.Text;
+
+                //params here
+                param = new SqlParameter("@lastName", SqlDbType.NChar);
+                param.Value = TextBoxLastName.Text; //variabel som blir sendt inn til metoden. Kommer fra bruker som tastet dette inn i et tekstfelt.
+                cmd.Parameters.Add(param);
+
+                
+
+                SqlDataReader reader = cmd.ExecuteReader();
+                dt.Load(reader);
+                reader.Close();
+                conn.Close();
+            }
+            GridView1.DataSource = dt;
+            GridView1.DataBind();
+        }
+
+        protected void ButtonSearchFirstName_Click(object sender, EventArgs e)
+        {
+            SqlParameter param;
+            var connectionString = ConfigurationManager.ConnectionStrings["ConnCms"].ConnectionString;
+            DataTable dt = new DataTable();
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+
+                SqlCommand cmd = new SqlCommand("SELECT * from Person where etternavn = @lastName", conn);
+                cmd.CommandType = CommandType.Text;
+
+                //params here
+                param = new SqlParameter("@lastName", SqlDbType.NChar);
+                param.Value = TextBoxLastName.Text; //variabel som blir sendt inn til metoden. Kommer fra bruker som tastet dette inn i et tekstfelt.
+                cmd.Parameters.Add(param);
+
+
+
+                SqlDataReader reader = cmd.ExecuteReader();
+                dt.Load(reader);
+                reader.Close();
+                conn.Close();
+            }
+            GridView1.DataSource = dt;
+            GridView1.DataBind();
+        }
+    }
+}
